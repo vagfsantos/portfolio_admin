9ac79a260a3da6faba489dcef6505ddd14f70dd5
Vagner Santos <santos.vagnerferreira@gmail.com>
2017-03-21 23:52:38 -0300

    modules structed

---
+++ inc/layout/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vagner Ferreira Santos | Admin</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div id="home" class="row">
+        <div class="container">
+
+          <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                  vagner santos
+                </a>
+              </div>
+
+              <ul class="nav navbar-nav navbar-right">
+                <li role="presentation"><a href="#home">Home</a></li>
+                <li role="presentation"><a href="#projetos">Projetos</a></li>
+                <li role="presentation"><a href="#habilidades">Habilidades</a></li>
+                <li role="presentation"><a href="#educacao">Educação</a></li>
+                <li role="presentation"><a href="#certificado">Certificado</a></li>
+                <li role="presentation"><a href="#experiencia">Experiência</a></li>
+              </ul>
+
+            </div>
+          </nav>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="jumbotron">
+          <div class="container">
+            <h2><a href="admin.php">Painel de administração</a></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur repellat modi sint, debitis harum! Unde quidem tempora, dolorum dicta rem veritatis est, consequatur numquam facilis ipsa fugit illum voluptate deleniti!</p>
+          </div>
+        </div>
+      </div>
